dfe0a706b1ca92572383f279f5b74c37b7bcd8e5
FiatNyx <43786615+FiatNyx@users.noreply.github.com>
2019-01-28 09:15:46 -0500

    Create PinceAHatch.java

---
+++ FRC/Subsystems/PinceAHatch.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Add your docs here.
+ */
+public class PinceAHatch extends Subsystem {
+  DoubleSolenoid hatcher = new DoubleSolenoid(4, 5);
+  
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void Prendre() {
+  hatcher.set(Value.kReverse);
+
+  }
+
+  public void Deposer() {
+  hatcher.set(Value.kForward);
+  
+  }
+
+
+}
+  
+
